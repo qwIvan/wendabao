fee28668210d909647cd7b70038fc927c2f8042b
yang chanfa <7613160+qwIvan@users.noreply.github.com>
2024-11-01 19:03:22 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('loaded')
+if (window.location.hostname==="ai.wendabeta.net") {
+    var iconSpan = document.querySelector('span.n-button__icon');
+    if (iconSpan) {
+        var parentElement = iconSpan.parentElement;
+        parentElement.addEventListener('click', function() {
+            var link = document.createElement('a');
+            link.href = 'https://ai.wendabao-a.net';
+            link.target = '_blank';
+            link.click();
+        });
+    }
+}
