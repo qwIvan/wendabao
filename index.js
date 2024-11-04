1ec2c5e6e697732ad2de30c811f8dcade28c460a
yang chanfa <7613160+qwIvan@users.noreply.github.com>
2024-11-04 14:42:48 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,5 @@
-setTimeout(function(){
-  if (window.location.hostname==="ai.wendabeta.net") {
-      var iconSpan = document.querySelector('span.n-button__icon');
-      if (iconSpan) {
-          var parentElement = iconSpan.parentElement;
-          parentElement.addEventListener('click', function() {
-              var link = document.createElement('a');
-              link.href = 'https://ai.wendabao-a.net/?utm_source=beta';
-              link.target = '_blank';
-              link.click();
-          });
-      }
-      var switchPanel = document.querySelector('div.switch-panel');
-      if (switchPanel) {
-          switchPanel.addEventListener('click', function(e) {
-              window.location.href = 'https://ai.wendabao-a.net/?utm_source=betaplus#/?role=PLUS'
-          });
-      }
-  }
-}, 10000)
+if (window.location.hostname==="ai.wendabeta.net") {
+    if (confirm("当前网址ai.wendabeta.net已停用，即将前往ai.wendabao.net继续使用")) {
+        window.location.href = 'https://ai.wendabao-a.net/?utm_source=betaconfirm#/?role=PLUS'
+    }
+}
