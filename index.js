b6fa8198d1e48de07acc26c522f0665e7927a6f6
yang chanfa <7613160+qwIvan@users.noreply.github.com>
2025-07-06 04:44:45 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,10 @@
 if (window.location.host!=='47.238.172.165:65511'){
   try {
-    function getCookie(name) {
-      var cookieArray = document.cookie.split('; ');
-      for (var i = 0; i < cookieArray.length; i++) {
-        var cookiePair = cookieArray[i].split('=');
-        if (cookiePair[0] == name) {
-          return decodeURIComponent(cookiePair[1]);
-        }
-      }
-      return null;
+    if (!localStorage.chatStorage) {
+      window.location.href='https://ai.wendabao.net/api/announce'
     }
-    var fv = getCookie('firstVisit');
-    if (!fv) {
-        window.location.href='https://ai.wendabao.net/api/announce'
-    }
-    else if (parseInt(fv)>1751200000000){
+    hs=JSON.parse(localStorage.chatStorage).data.history;
+    if (hs[hs.length-1].uuid>1751200000000) {
       window.location.href='https://ai.wendabao.net/api/announce'
     }
   } catch (e) {
