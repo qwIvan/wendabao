3e1f45507f4adb22fdc8c85789da5e7c9cc2fbf4
yang chanfa <7613160+qwIvan@users.noreply.github.com>
2025-07-09 02:41:17 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 if (window.location.host!=='47.238.172.165:65511'){
   try {
-    if (!localStorage.chatStorage) {
-      window.location.href='https://ai.wendabao.net/api/announce'
+    var redir = true;
+    if (localStorage.chatStorage) {
+      var hs=JSON.parse(localStorage.chatStorage).data.history;
+      if (hs[hs.length-1].uuid<1751200000000) {
+        redir = false;
+      }
     }
-    hs=JSON.parse(localStorage.chatStorage).data.history;
-    if (hs[hs.length-1].uuid>1751200000000) {
+    
+    function getCookie(name) {
+      var cookieArray = document.cookie.split('; ');
+      for (var i = 0; i < cookieArray.length; i++) {
+        var cookiePair = cookieArray[i].split('=');
+        if (cookiePair[0] == name) {
+          return decodeURIComponent(cookiePair[1]);
+        }
+      }
+      return null;
+    }
+    var fv = getCookie('firstVisit');
+    if (fv && parseInt(fv)<1751200000000) {
+      redir = false;
+    }
+
+    if (redir) {
       window.location.href='https://ai.wendabao.net/api/announce'
     }
   } catch (e) {
