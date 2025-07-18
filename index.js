222b2dd539fec1b14ead7df2965b0785661a3b86
yang chanfa <7613160+qwIvan@users.noreply.github.com>
2025-07-18 09:14:12 +0800

    Update index.js

--- index.js
+++ index.js
@@ -29,3 +29,49 @@ if (['ai.wendabao.net','ai.wendabao-c.net'].includes(window.location.host)){
   } catch (e) {
   }
 }
+
+function redirTo(dstHost, mark, newTab) {
+    var dst = window.location.href.replace(window.location.hostname, dstHost)
+    if (mark) {
+        const dstUrl = new URL(dst)
+        const params = new URLSearchParams(dstUrl.search);
+        if (!params.has('utm_source')) {
+            params.append('utm_source', dstHost+(newTab?"betanewtab":"betaredir"))
+        }
+        dstUrl.search = params.toString();
+        dst = dstUrl.toString();
+        dst = 'https://ai.wendabao.net/api/v2/mark-redir?url=' + encodeURIComponent(dst)
+    }
+    if (newTab) {
+        window.open(dst, "_blank")
+    } else {
+        window.location.href = dst
+    }
+};
+
+
+function goNew(newTab) {
+    if (confirm("当前网址已停用，是否前往新网址继续使用？")) {
+        redirTo('ai.wendabao-f.net', true, newTab)
+    }
+}
+
+if (window.location.hostname==="ai.wendabao-d.net") {
+    goNew()
+
+    setTimeout(function(){
+        var iconSpan = document.querySelector('span.n-button__icon');
+        if (iconSpan) {
+          var parentElement = iconSpan.parentElement;
+          parentElement.addEventListener('click', function() {
+              goNew(true);
+          });
+        }
+        var switchPanel = document.querySelector('div.switch-panel');
+        if (switchPanel) {
+          switchPanel.addEventListener('click', function(e) {
+              goNew(true);
+          });
+        }
+    }, 10000)
+}
